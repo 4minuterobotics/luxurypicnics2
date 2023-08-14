90c2db737f9a62e4dd370d3137c99de6aef96d71
William Lawrence <williamlawrence@Williams-Air.jetpack>
2023-08-14 19:39:08 -0400

    added new pics, made grid on home page look better and populated with actual pics, updated packages, updated and populated picnic pics and info

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,30 +1,49 @@
-import banner from './banner.jpeg'
+import banner from './banner.jpeg';
 import logoCloudBg from './logo_cloud_bg.png';
 import redLuxuryText from './red-luxury-text.png';
 import maroonLuxuryText from './maroon-luxury-text.png';
 import preview from './preview.png';
 import number1 from './numbers-1.png';
 import number2 from './numbers-2.png';
-import number3 from './numbers-3.png'; 
+import number3 from './numbers-3.png';
 import grayBGStrip from './gray-bg-strip.png';
 import navIcons1 from './nav-icon1.svg';
 import navIcons2 from './nav-icon2.svg';
 import navIcons3 from './nav-icon3.svg';
 import navbarLogo from './navbar-logo.png';
+import romantic1 from './romantic1.jpeg';
+import romantic2 from './romantic2.jpeg';
+import romantic3 from './romantic3.jpeg';
+import romantic4 from './romantic4.jpeg';
+import romantic5 from './romantic5.jpeg';
+import romantic6 from './romantic6.jpeg';
+import romantic7 from './romantic7.jpeg';
+import romantic8 from './romantic8.jpeg';
+import romantic9 from './romantic9.jpeg';
+import romantic10 from './romantic10.jpeg';
 
-
-export { 
-    banner,
-    logoCloudBg,
-    redLuxuryText,
-    maroonLuxuryText,
-    preview,
-    number1,
-    number2,
-    number3,
-    grayBGStrip,
-    navIcons1,
-    navIcons2,
-    navIcons3,
-    navbarLogo
-}; 
+export {
+	banner,
+	logoCloudBg,
+	redLuxuryText,
+	maroonLuxuryText,
+	preview,
+	number1,
+	number2,
+	number3,
+	grayBGStrip,
+	navIcons1,
+	navIcons2,
+	navIcons3,
+	navbarLogo,
+	romantic1,
+	romantic2,
+	romantic3,
+	romantic4,
+	romantic5,
+	romantic6,
+	romantic7,
+	romantic8,
+	romantic9,
+	romantic10,
+};
--- src/containers/photo-grid/PhotoGrid.jsx
+++ src/containers/photo-grid/PhotoGrid.jsx
@@ -1,49 +1,88 @@
-import React from 'react'
+import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'; 
+import Col from 'react-bootstrap/Col';
 
-import { navbarLogo } from '../../assets';
+import {
+	navbarLogo,
+	romantic1,
+	romantic10,
+	romantic2,
+	romantic3,
+	romantic4,
+	romantic5,
+	romantic6,
+	romantic7,
+	romantic8,
+	romantic9,
+} from '../../assets';
 
-import './photoGrid.css'
+import './photoGrid.css';
 const PhotoGrid = () => {
-    return (
-        <Container className="photo-grid">
-            {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
-            <Row>
-                <Col xs={8} md={4}>
-                    <img src= {navbarLogo}/>
-                </Col>
-                <Col xs={4} md={4}>
-                    <img src= {navbarLogo}/>
-                </Col>
-                <Col xs={5} md={4}>
-                    <img src= {navbarLogo}/>
-                </Col>
-                
-                <Col xs={7} md={4}>
-                    <img src= {navbarLogo}/>
-                </Col>
-                <Col xs={12} md={4}>
-                    <img src= {navbarLogo}/>
-                </Col>
-                <Col xs={5} md={4}>
-                    <img src= {navbarLogo}/>
-                </Col>
+	return (
+		<Container className='photo-grid'>
+			{/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+			<Row className='photogrid-photos'>
+				<Col
+					xs={8}
+					md={4}
+				>
+					<img src={romantic1} />
+				</Col>
+				<Col
+					xs={4}
+					md={4}
+				>
+					<img src={romantic2} />
+				</Col>
+				<Col
+					xs={5}
+					md={4}
+				>
+					<img src={romantic3} />
+				</Col>
 
-                <Col xs={7} md={4}>
-                    <img src= {navbarLogo}/>
-                </Col>
-                <Col xs={8} md={4}>
-                    <img src= {navbarLogo}/>
-                </Col>
-                <Col xs={4} md={4}>
-                    <img src= {navbarLogo}/>
-                </Col>
-            </Row>
-        </Container>
-  )
-}
+				<Col
+					xs={7}
+					md={4}
+				>
+					<img src={romantic4} />
+				</Col>
+				<Col
+					xs={12}
+					md={4}
+				>
+					<img src={romantic5} />
+				</Col>
+				<Col
+					xs={5}
+					md={4}
+				>
+					<img src={romantic6} />
+				</Col>
 
-export default PhotoGrid
+				<Col
+					xs={7}
+					md={4}
+				>
+					<img src={romantic7} />
+				</Col>
+				<Col
+					xs={8}
+					md={4}
+				>
+					<img src={romantic9} />
+				</Col>
+				<Col
+					xs={4}
+					md={4}
+				>
+					<img src={romantic10} />
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+export default PhotoGrid;
