7ef8aae9a73c78b82a436d538b7c89aebcc3df7d
William Lawrence <williamlawrence@Williams-Air.jetpack>
2023-08-18 11:01:21 -0400

    made navbar collapse when menu item is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,99 @@
 import React, { useState } from 'react';
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from 'react-router-dom';
 // import { AnimatePresence } from 'framer-motion';
 import { navbarContext } from './contexts/navbarContext';
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar, PageLoadingScreen, ScrollToTop } from './components';
 import { Footer } from './containers';
 
 // USING LAZY LOADING INSTEAD!!
-// import { Home, Book, Picnics, Parties, BeachSupplies } from './pages'; 
+// import { Home, Book, Picnics, Parties, BeachSupplies } from './pages';
 
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 //Imports for lazy loading pages
-const LazyHome = React.lazy(()=> import ('./pages/home/Home'))
-const LazyPicnics = React.lazy(()=> import ('./pages/picnics/Picnics'))
-const LazyParties = React.lazy(()=> import ('./pages/parties/Parties'))
-const LazyBeachSupplies = React.lazy(()=> import ('./pages/beach-supplies/BeachSupplies'))
-const LazyBook = React.lazy(()=> import ('./pages/book/Book'))
+const LazyHome = React.lazy(() => import('./pages/home/Home'));
+const LazyPicnics = React.lazy(() => import('./pages/picnics/Picnics'));
+const LazyParties = React.lazy(() => import('./pages/parties/Parties'));
+const LazyBeachSupplies = React.lazy(() => import('./pages/beach-supplies/BeachSupplies'));
+const LazyBook = React.lazy(() => import('./pages/book/Book'));
 
 function App() {
-  // const location = useLocation();
+	// const location = useLocation();
 
-  const [activeLink, setActiveLink] = useState('home');
+	const [activeLink, setActiveLink] = useState('home');
 
-  return (
-    <>
-      <div className="App">
-      <navbarContext.Provider value={({activeLink, setActiveLink})}>
-        <ToastContainer position ="bottom-center" limit={1}/>
-        <ScrollToTop/>
-        <NavBar/>      
-        {/* <AnimatePresence mode="wait"> */}
-          <Routes key={location.pathname} location={location}>
+	return (
+		<>
+			<div className='App'>
+				<navbarContext.Provider value={{ activeLink, setActiveLink }}>
+					<ToastContainer position='bottom-center' limit={1} />
+					<ScrollToTop />
+					<NavBar />
+					{/* <AnimatePresence mode="wait"> */}
+					<Routes key={location.pathname} location={location}>
+						{/* lazy loading routhing method */}
+						<Route
+							path='/'
+							element={
+								<React.Suspense fallback={<PageLoadingScreen />}>
+									{' '}
+									<LazyHome />{' '}
+								</React.Suspense>
+							}
+						/>
+						<Route
+							path='/parties'
+							element={
+								<React.Suspense fallback={<PageLoadingScreen />}>
+									{' '}
+									<LazyParties />{' '}
+								</React.Suspense>
+							}
+						/>
+						<Route
+							path='/picnics'
+							element={
+								<React.Suspense fallback={<PageLoadingScreen />}>
+									{' '}
+									<LazyPicnics />{' '}
+								</React.Suspense>
+							}
+						/>
+						<Route
+							path='/beachSupplies'
+							element={
+								<React.Suspense fallback={<PageLoadingScreen />}>
+									{' '}
+									<LazyBeachSupplies />{' '}
+								</React.Suspense>
+							}
+						/>
+						<Route
+							path='/book'
+							element={
+								<React.Suspense fallback={<PageLoadingScreen />}>
+									{' '}
+									<LazyBook />{' '}
+								</React.Suspense>
+							}
+						/>
 
-            {/* lazy loading routhing method */}
-            <Route path="/" element={<React.Suspense fallback= {<PageLoadingScreen/>} > <LazyHome/> </React.Suspense>}/>  
-            <Route path="/parties" element={<React.Suspense fallback= {<PageLoadingScreen/>} > <LazyParties/> </React.Suspense>}/>  
-            <Route path="/picnics" element={<React.Suspense fallback= {<PageLoadingScreen/>} > <LazyPicnics/> </React.Suspense>}/>  
-            <Route path="/beachSupplies" element={<React.Suspense fallback= {<PageLoadingScreen/>} > <LazyBeachSupplies/> </React.Suspense>}/>  
-            <Route path="/book" element={<React.Suspense fallback= {<PageLoadingScreen/>} > <LazyBook/> </React.Suspense>}/>   
-
-            {/* non-lazy loading routing method below*/}
-            {/* <Route path="/" element={<Home/>}/>  
+						{/* non-lazy loading routing method below*/}
+						{/* <Route path="/" element={<Home/>}/>  
             <Route path="/parties" element={<Parties/>}/>  
             <Route path="/picnics" element={<Picnics/>}/>  
             <Route path="/beachSupplies" element={<BeachSupplies/>}/>  
             <Route path="/book" element={<Book/>}/>   */}
-          </Routes> 
-        {/* </AnimatePresence>            */}
-        <Footer/>
-      </navbarContext.Provider>             
-      </div>
-    </>
-  )
+					</Routes>
+					{/* </AnimatePresence>            */}
+					<Footer />
+				</navbarContext.Provider>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,91 +1,97 @@
-import React from 'react'
-import { useState, useEffect, useContext } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import React from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import './navbar.css';
 import { navbarLinks } from '../../objects';
-import navLogo from '../../assets/navbar-logo.png'
-import { Link } from "react-router-dom";
+import navLogo from '../../assets/navbar-logo.png';
+import { Link } from 'react-router-dom';
 import { navbarContext } from '../../contexts/navbarContext';
 
 const NavBar = () => {
+	const { activeLink, setActiveLink } = useContext(navbarContext);
 
-    const { activeLink, setActiveLink } = useContext(navbarContext);
-    
-    const [scrolled, setScrolled] = useState(false);
+	const [scrolled, setScrolled] = useState(false);
+	const [expanded, setExpanded] = useState(false);
 
+	useEffect(() => {
+		const onScroll = () => {
+			if (window.scrollY > 50) {
+				setScrolled(true);
+			} else {
+				setScrolled(false);
+			}
+		};
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-            setScrolled(true);
-        } else {
-            setScrolled(false);
-            }
-        }
+		window.addEventListener('scroll', onScroll);
 
+		return () => window.removeEventListener('scroll', onScroll);
+	}, []);
 
-        window.addEventListener("scroll", onScroll);
+	const handleClick = (value) => {
+		setActiveLink(value);
+		setExpanded(false);
+		console.log('going to page section ', value);
+	};
+	return (
+		<>
+			<Navbar expand='md' className={scrolled ? 'scrolled' : ''} expanded={expanded}>
+				<Container>
+					<Navbar.Brand className='nav-logo'>
+						<Link to='/'>
+							<img src={navLogo} alt='Logo' />
+						</Link>
+					</Navbar.Brand>
 
-        return () => window.removeEventListener("scroll", onScroll);
-    }, [])
-
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
-        console.log('going to page section ', value)
-    }
-    return (
-        <>
-            <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-                <Container>
-                    <Navbar.Brand className ="nav-logo">
-                        <Link to="/">
-                            <img src={navLogo} alt="Logo" />
-                        </Link>  
-                    </Navbar.Brand>
-
-                    {/* <Navbar.Brand href="/">
+					{/* <Navbar.Brand href="/">
                         <img src={navLogo} alt="Logo" />
                     </Navbar.Brand> */}
-                    
-                    <Navbar.Toggle aria-controls="basic-navbar-nav">
-                        <span className="navbar-toggler-icon"></span>
-                    </Navbar.Toggle>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto">
-                            {navbarLinks.map((link)=>{
-                                return(
-                                    <Link key={link.id} to={`/${link.hyperlink}`} className ={activeLink === link.hyperlink ? 'active nav-links' : 'nav-links'} onClick={() => onUpdateActiveLink(link.hyperlink)}>{link.title}</Link>
-                                )
-                            })}
 
-                            {/* <Link to="/" className ={activeLink === 'home' ? 'active nav-links' : 'nav-links'} onClick={() => onUpdateActiveLink('home')}>Home</Link>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' onClick={() => setExpanded(expanded ? false : 'expanded')}>
+						<span className='navbar-toggler-icon'></span>
+					</Navbar.Toggle>
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ms-auto'>
+							{navbarLinks.map((link) => {
+								return (
+									<Link
+										key={link.id}
+										to={`/${link.hyperlink}`}
+										className={activeLink === link.hyperlink ? 'active nav-links' : 'nav-links'}
+										onClick={() => handleClick(link.hyperlink)}
+									>
+										{link.title}
+									</Link>
+								);
+							})}
+
+							{/* <Link to="/" className ={activeLink === 'home' ? 'active nav-links' : 'nav-links'} onClick={() => onUpdateActiveLink('home')}>Home</Link>
                             <Link to="/partyOptions" className ={activeLink === 'partyOptions' ? 'active nav-links' : 'nav-links'} onClick={() => onUpdateActiveLink('partyOptions')}>Party Options</Link>
                             <Link to="/about" className ={activeLink === 'about' ? 'active nav-links' : 'nav-links'} onClick={() => onUpdateActiveLink('about')}>About</Link>
                             <Link to="/book" className ={activeLink === 'book' ? 'active nav-links' : 'nav-links'} onClick={() => onUpdateActiveLink('book')}>Book</Link>                            */}
-                            
-                            {/* <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+
+							{/* <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
                             <Nav.Link href="#technologies" className={activeLink === 'technologies' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('technologies')}>Party Styles</Nav.Link>
                             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>FAQs</Nav.Link>
                             <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>About</Nav.Link>
                             <Nav.Link href="#book" className={activeLink === 'book' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('book')}>Book Now</Nav.Link> */}
-                        </Nav>
+						</Nav>
 
-                        
-                        {/* <span className="navbar-text">
+						{/* <span className="navbar-text">
                             <div className="social-icon">
                                 <a href="https://www.instagram.com/MonsterBouncyHouses/"><img src={navIcon3} alt="" /></a>
                             </div>
                         </span> */}
-
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </> 
-    )
-}
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
+	);
+};
 
 export default NavBar;
 
-{/* <Link className ="nav-link" to="/signin">
+{
+	/* <Link className ="nav-link" to="/signin">
     Sign In
-</Link> */}
+</Link> */
+}
