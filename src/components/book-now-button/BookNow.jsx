a6b3b6048f536f2a87141e77388647b3dcbfbaed
William Lawrence <lawrence.william86@gmail.com>
2025-04-14 13:46:42 -0400

    updated name of bookNow.css page

--- src/components/book-now-button/BookNow.jsx
+++ src/components/book-now-button/BookNow.jsx
@@ -1,21 +1,25 @@
-import React, { useContext } from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, { useContext } from 'react';
 import './bookNow.css';
 
 import { Link } from 'react-router-dom';
 import { navbarContext } from '../../contexts/navbarContext';
 
-
 const BookNow = () => {
+	const { setActiveLink } = useContext(navbarContext);
 
-  const { setActiveLink } = useContext(navbarContext);
+	return (
+		<div className='book-now'>
+			<Link
+				to='/book'
+				onClick={() => setActiveLink('book')}
+			>
+				<button>
+					<span>Book Now</span>
+				</button>
+			</Link>
+		</div>
+	);
+};
 
-  return (
-    <div className="book-now">
-      <Link to="/book" onClick={() => setActiveLink("book")}>
-      <button><span>Book Now</span></button>
-      </Link>                            
-    </div>
-  )
-}
-
-export default BookNow
+export default BookNow;
